f85e570d008887a884f474ff99ce31411d84c85d
Kanishka Goyal <127941423+goyalkan@users.noreply.github.com>
2023-08-02 20:42:10 +0530

    Create BMI calculator.py

---
+++ BMI calculator.py
@@ -0,0 +1,11 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+
+
+
+nheight = float(height)
+nweight = int(weight)
+
+a = (nweight/(nheight*nheight))
+newa = int(a)
+print(newa)
